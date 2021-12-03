2cee3edf04fb77e46a224d596fb4773d99c6fbde
JariMiers04 <56120363+JariMiers04@users.noreply.github.com>
2021-12-03 17:39:57 +0100

    refactor: added extra file for cleaner code

--- api/src/database/migrate.js
+++ api/src/database/migrate.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const KNEX = require('knex')({
-    client: "pg",
-    connection: {
-        host: process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : 'localhost',
-        port: 5432,
-        user: process.env.POSTGRES_USER ? process.env.POSTGRES_USER : "root",
-        password: process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : 'root',
-        database: process.env.POSTGRES_DB ? process.env.POSTGRES_DB : "foodStock"
-    }
-});
-
+const {KNEX} = require("../knex");
 const MIGRATE = {
 
     async makeTables(){
