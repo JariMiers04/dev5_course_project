c8b18434c00b645c2c6460062866fa2c03544f62
JariMiers04 <56120363+JariMiers04@users.noreply.github.com>
2021-12-04 14:05:59 +0100

    fix: set table creation in try catch block

--- api/src/database/migrate.js
+++ api/src/database/migrate.js
@@ -5,35 +5,41 @@ const MIGRATE = {
 
     async makeTables(){
 
-        await KNEX.schema.hasTable("food").then(async function (exists) {
-            if (!exists) {
-                return await KNEX.schema.createTable('food', (table) => {
-                    table.double('barcode', 12).primary();
-                    table.string('product_name').notNullable();
-                    table.date('expiration_date').notNullable();
-                    table.integer('weight').notNullable();
-                })
-            }
-        });
+        try{
+            await KNEX.schema.hasTable("food").then(async (exists) => {
+                if (!exists) {
+                    return await KNEX.schema.createTable('food', (table) => {
+                        table.double('barcode', 12).primary();
+                        table.string('product_name').notNullable();
+                        table.date('expiration_date').notNullable();
+                        table.integer('weight').notNullable();
+                    })
+                }
+            });
+    
+            await KNEX.schema.hasTable("users").then(async (exists)=>{
+                if(!exists){
+                    return await KNEX.schema.createTable('users', (table) => {
+                        table.increments('id').primary();
+                        table.string('name').notNullable();
+                        table.string('email').notNullable();
+                        table.string('password').notNullable();
+                    })
+                }
+            });
+    
+            await KNEX.schema.hasTable("fridge").then(async (exists)=>{
+                if(!exists){
+                    return await KNEX.schema.createTable('fridge', (table) => {
+                        table.increments('id').primary();
+                    })
+                }
+            });
+        }catch(err){
+            console.log(err);
+            process.exit(1);
+        }
 
-        await KNEX.schema.hasTable("users").then(async function(exists){
-            if(!exists){
-                return await KNEX.schema.createTable('users', (table) => {
-                    table.increments('id').primary();
-                    table.string('name').notNullable();
-                    table.string('email').notNullable();
-                    table.string('password').notNullable();
-                })
-            }
-        });
-
-        await KNEX.schema.hasTable("fridge").then(async function(exists){
-            if(!exists){
-                return await KNEX.schema.createTable('fridge', (table) => {
-                    table.increments('id').primary();
-                })
-            }
-        });
 
     }
 }
