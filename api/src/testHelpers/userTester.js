68b12621cea21571a5ba9eceb7f91680439fb8cc
JariMiers04 <56120363+JariMiers04@users.noreply.github.com>
2021-12-21 21:47:38 +0100

    feat(user): working user testing

---
+++ api/src/testHelpers/userTester.js
@@ -0,0 +1,37 @@
+function checkPostUser(user){
+    if(!user || !user.name || !user.email || !user.password){
+        return false
+    } if(!checkUserName(user.name) || !checkUserEmail(user.email) || !checkUserPassword(user.password)){
+        return false
+    }
+    else{
+        return true
+    }
+}
+
+function checkUserName(name){
+    if(!name || !name.length){
+        return false
+    }else{
+        return true
+    }
+}
+
+function checkUserEmail(email){
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(emailRegex)){
+        return true
+    }else{
+        return false
+    }
+}
+
+function checkUserPassword(password){
+    if(!password || !password.length){
+        return false
+    }else{
+        return true
+    }
+}
+
+module.exports = {checkPostUser, checkUserName, checkUserEmail, checkUserPassword}
